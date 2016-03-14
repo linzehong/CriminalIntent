8a380e58bf1296e5a2cf9d38a019d6feb9f830cb
linzehong <linzehong@cvte.com>
2016-03-14 19:46:18 +0800

    [feature] 创建工程CriminalIntent

---
+++ app/src/main/java/com/cvte/criminalintent/Crime.java
@@ -0,0 +1,32 @@
+package com.cvte.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Administrator on 2016/3/14.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
